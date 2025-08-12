1207df80fdafa0db33da7d3125ba40579046c90d
enzeberg <enzeberg@outlook.com>
2025-08-12 14:54:45 +0800

    style: format code

--- src/pages/index/App.jsx
+++ src/pages/index/App.jsx
@@ -1,29 +1,24 @@
-import { connect } from 'react-redux';
-import { Layout } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import TheHeader from './components/Header';
-import SearchResult from './components/SearchResult';
-import TopSongs from './components/TopSongs';
-import Player from './components/Player';
-import Hot from './components/Hot';
-import './App.css';
-const { Header, Content } = Layout;
+import { connect } from 'react-redux'
+import { Layout } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import TheHeader from './components/Header'
+import SearchResult from './components/SearchResult'
+import TopSongs from './components/TopSongs'
+import Player from './components/Player'
+import Hot from './components/Hot'
+import './App.css'
+const { Header, Content } = Layout
 
 function App({ searchStatus, searchResults }) {
   return (
     <BrowserRouter>
-      <Layout
-        style={{
-          backgroundColor: '#f7f7f7',
-        }}
-      >
+      <Layout>
         <Header
           style={{
             position: 'fixed',
             width: '100%',
             zIndex: 1040,
-            // borderBottom: '1px solid #DBDBDB',
             padding: '8px 0',
             boxShadow: '0 1px 3px rgba(26,26,26,.1)',
           }}
@@ -31,7 +26,8 @@ function App({ searchStatus, searchResults }) {
           <TheHeader />
         </Header>
         <Content>
-          <div className="container"
+          <div
+            className='container'
             style={{
               marginTop: 59,
               marginBottom: 74,
@@ -39,42 +35,33 @@ function App({ searchStatus, searchResults }) {
             }}
           >
             <Switch>
-              <Route exact path="/" component={Hot} />
-              <Route path="/search"
-                render={
-                  () => {
-                    const filtered = Object.keys(searchResults)
-                      .filter(key => {
-                        const result = searchResults[key];
-                        return result.searchSuccess &&
-                          result.data.totalCount > 0;
-                      });
-                    return (
-                      <>
-                        <TopSongs />
-                        {
-                          filtered.map((key) => (
-                            <SearchResult
-                              result={searchResults[key]}
-                              provider={key}
-                              key={key}
-                            />
-                          ))
-                        }
-                        {
-                          filtered.length === 0 && searchStatus === 'done' &&
-                          <div className="white-card">
-                            抱歉，未搜索到相关内容。
-                          </div>
-                        }
-                        {
-                          searchStatus === 'searching' &&
-                          <LoadingOutlined />
-                        }
-                      </>
-                    );
-                  }
-                }
+              <Route exact path='/' component={Hot} />
+              <Route
+                path='/search'
+                render={() => {
+                  const filtered = Object.keys(searchResults).filter((key) => {
+                    const result = searchResults[key]
+                    return result.searchSuccess && result.data.totalCount > 0
+                  })
+                  return (
+                    <>
+                      <TopSongs />
+                      {filtered.map((key) => (
+                        <SearchResult
+                          result={searchResults[key]}
+                          provider={key}
+                          key={key}
+                        />
+                      ))}
+                      {filtered.length === 0 && searchStatus === 'done' && (
+                        <div className='white-card'>
+                          抱歉，未搜索到相关内容。
+                        </div>
+                      )}
+                      {searchStatus === 'searching' && <LoadingOutlined />}
+                    </>
+                  )
+                }}
               />
             </Switch>
           </div>
@@ -82,14 +69,14 @@ function App({ searchStatus, searchResults }) {
         <Player />
       </Layout>
     </BrowserRouter>
-  );
+  )
 }
 
 function mapStateToProps(state) {
   return {
     searchStatus: state.searchStatus,
     searchResults: state.searchResults,
-  };
+  }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
--- src/pages/index/components/TopSongs.jsx
+++ src/pages/index/components/TopSongs.jsx
@@ -1,51 +1,51 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import SongList from './SongList';
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import SongList from './SongList'
 
 class TopSongs extends Component {
   constructor() {
     this.state = {
-      topSongs: []
-    };
+      topSongs: [],
+    }
   }
 
   componentDidUpdate(prevProps) {
-    const currentResults = this.props.searchResults;
+    const currentResults = this.props.searchResults
     if (currentResults !== prevProps.searchResults) {
-      // changing search keyword will cause 'CLEAR_RESULTS', which means the 
-      // topSongs should be cleared too.
+      // changing search keyword will cause 'CLEAR_RESULTS', which means the topSongs should be cleared too.
       if (Object.keys(currentResults).length === 0) {
         this.setState({
-          topSongs: []
-        });
+          topSongs: [],
+        })
       }
       Object.keys(currentResults).forEach((key) => {
         if (this.state.topSongs.every((song) => song.platform !== key)) {
           if (currentResults[key].searchSuccess) {
             this.setState({
-              topSongs: [...this.state.topSongs,
-              currentResults[key].data.songs[0]
-              ]
-            });
+              topSongs: [
+                ...this.state.topSongs,
+                currentResults[key].data.songs[0],
+              ],
+            })
           }
         }
-      });
+      })
     }
   }
 
   render() {
     return this.state.topSongs.length === 0 ? null : (
-      <div className="white-card">
+      <div className='white-card'>
         <SongList songs={this.state.topSongs} />
       </div>
-    );
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
-    searchResults: state.searchResults
-  };
+    searchResults: state.searchResults,
+  }
 }
 
-export default connect(mapStateToProps)(TopSongs);
+export default connect(mapStateToProps)(TopSongs)
