c6530ed377e7c9187dd488c980ccb03764f10e2e
李恩泽 (Li Enze) <enzeberg@outlook.com>
2025-08-13 09:59:45 +0800

    refactor(SearchResult): optimize with useCallback and useMemo

--- src/pages/index/components/SearchResult.jsx
+++ src/pages/index/components/SearchResult.jsx
@@ -1,51 +1,68 @@
-import { Pagination } from 'antd';
-import { connect } from 'react-redux';
-import SongList from './SongList';
-import Wrapper from './Wrapper';
-import OperatingBarOfSongList from './OperatingBarOfSongList';
+import { Pagination } from 'antd'
+import { connect } from 'react-redux'
+import { useCallback, useMemo } from 'react'
+import SongList from './SongList'
+import Wrapper from './Wrapper'
+import OperatingBarOfSongList from './OperatingBarOfSongList'
 
 function SearchResult({ result, provider, keyword, onResultResponded }) {
-  function onPageChange() {
-    fetch(`/api/search?provider=${provider}&keyword=${keyword}&page=${page}`)
-      .then(res => res.json())
-      .then(json => {
-        onResultResponded(provider, json);
-      })
-      .catch(err => {
-        console.log('err ', err);
-      });
+  // 使用 useCallback 优化函数，避免不必要的重新渲染
+  const onPageChange = useCallback(
+    (page) => {
+      fetch(`/api/search?provider=${provider}&keyword=${keyword}&page=${page}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`)
+          }
+          return res.json()
+        })
+        .then((json) => {
+          onResultResponded(provider, json)
+        })
+        .catch((err) => {
+          console.error('搜索请求失败:', err)
+        })
+    },
+    [provider, keyword, onResultResponded]
+  )
+
+  const songs = useMemo(() => result?.data?.songs || [], [result])
+  const totalCount = useMemo(() => result?.data?.totalCount || 0, [result])
+
+  if (!result || !result.data) {
+    return null
   }
 
   return (
-    <Wrapper provider={provider}
-      operatingBar={
-        <OperatingBarOfSongList songs={result.data.songs} />
-      }
+    <Wrapper
+      provider={provider}
+      operatingBar={<OperatingBarOfSongList songs={songs} />}
       pagination={
         <Pagination
           simple
           onChange={onPageChange}
           defaultPageSize={4}
-          total={result.data.totalCount}
+          total={totalCount}
+          showSizeChanger={false}
         />
       }
     >
-      <SongList songs={result.data.songs} />
+      <SongList songs={songs} />
     </Wrapper>
-  );
+  )
 }
 
 function mapStateToProps(state) {
   return {
     keyword: state.searchKeyword,
-  };
+  }
 }
 function mapDispatchToProps(dispatch) {
   return {
     onResultResponded: (provider, data) => {
-      dispatch({ type: 'UPDATE_SEARCH_RESULTS', provider, data});
-    }
-  };
+      dispatch({ type: 'UPDATE_SEARCH_RESULTS', provider, data })
+    },
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchResult);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResult)
