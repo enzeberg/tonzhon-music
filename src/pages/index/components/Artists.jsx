0c75b215fdc28ad453164b78299997c90c05d070
李恩泽 (Li Enze) <enzeberg@outlook.com>
2025-08-13 11:18:52 +0800

    style(Artists)

--- src/pages/index/components/Artists.jsx
+++ src/pages/index/components/Artists.jsx
@@ -1,12 +1,12 @@
-import { Fragment } from 'react';
+import { Fragment } from 'react'
 
 export default function Artists({ artists }) {
   return artists.map((artist) => {
-      return (
-        <Fragment key={artist.id}>
-          {artist.name}
-          &ensp;
-        </Fragment>
-      );
-    });
-};
+    return (
+      <Fragment key={artist.id}>
+        {artist.name}
+        &ensp;
+      </Fragment>
+    )
+  })
+}
